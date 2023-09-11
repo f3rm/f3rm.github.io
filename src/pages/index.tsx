72861d5e0ee1153e45da88ca0788a5e624c4493c
William Shen <william.w.y.shen@gmail.com>
2023-09-11 16:49:58 -0400

    Remove "coming soon" from code button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -211,8 +211,7 @@ const IndexPage: React.FC<PageProps> = () => {
                     <p className="flex flex-wrap justify-center">
                         <ActionLink url={"https://arxiv.org/abs/2308.07931"} icon={<FaFilePdf/>}>Paper</ActionLink>
                         <ActionLink url={"#video"} icon={<FaVideo/>}>Video</ActionLink>
-                        <ActionLink url={"https://github.com/f3rm/f3rm"} icon={<AiFillGithub/>}>Code (coming
-                            soon)</ActionLink>
+                        <ActionLink url={"https://github.com/f3rm/f3rm"} icon={<AiFillGithub/>}>Code</ActionLink>
                     </p>
 
                     {/* Teaser Video */}
